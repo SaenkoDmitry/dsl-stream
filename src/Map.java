328d1230070867a1aeb5205fa257de7a41fa7c1c
SaenkoDmitry <dmitryssaenko@gmail.com>
2018-04-06 17:26:29 +0300

    Update Map.java

--- src/Map.java
+++ src/Map.java
@@ -1,17 +1,17 @@
 import java.util.Iterator;
 import java.util.function.Function;
 
-public class Map<T> extends Source<T> {
+public class Map<T, R> implements Source<R> {
     private Source<T> source;
-    private Function<T, T> function;
+    private Function<T, R> function;
 
-    public Map(Source<T> source, Function<T, T> function) {
+    public Map(Source<T> source, Function<T, R> function) {
         this.source = source;
         this.function = function;
     }
 
     @Override
-    public T get() {
+    public R get() {
         T item = source.get();
         if (item != null)
             return function.apply(item);
