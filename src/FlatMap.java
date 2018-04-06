80084b445fb5638cae140bbcfed14f63dee6b5aa
SaenkoDmitry <dmitryssaenko@gmail.com>
2018-04-06 17:25:57 +0300

    Update FlatMap.java

--- src/FlatMap.java
+++ src/FlatMap.java
@@ -1,19 +1,28 @@
 import java.util.function.Function;
 
-public class FlatMap<T> extends Source<T> {
+public class FlatMap<T, R> implements Source<R> {
     private Source<T> source;
-    private Function<T, DSL<? extends T>> function;
+    private Source<R> sourceFlatted;
+    private Function<T, Source<R>> function;
 
-    public FlatMap(Source<T> source, Function<T, DSL<? extends T>> function) {
+    public FlatMap(Source<T> source, Function<T, Source<R>> function) {
         this.source = source;
         this.function = function;
+        this.sourceFlatted = function.apply(source.get());
     }
 
     @Override
-    public T get() {
-        T item = source.get();
-//        if (item != null)
-//            return function.apply(item);
-        return null;
+    public R get() {
+        while (true) {
+            R item = sourceFlatted.get();
+            if (item != null) {
+                return item;
+            } else {
+                T item1 = source.get();
+                if (item1 == null)
+                    return null;
+                sourceFlatted = function.apply(item1);
+            }
+        }
     }
 }
